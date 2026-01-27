0cbf20096ff53861258a74ecaf6216ab753d76c6
Rizwan Fahim <rizwanfahim011@outlook.com>
2026-01-27 22:05:46 +0500

    Update Socket.js

--- backend/Socket/Socket.js
+++ backend/Socket/Socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin:"https://instagram-clone-bnpm-git-main-gitrizwan-webs-projects.vercel.app/" || "http://localhost:5173",
     methods: ["GET", "POST"],
     credentials: true,
   },
