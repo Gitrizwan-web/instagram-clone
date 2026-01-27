a97ccd85dde660929c20be67ab17167a2bc68421
Rizwan Fahim <rizwanfahim011@outlook.com>
2026-01-27 21:57:08 +0500

    Update Socket.js

--- backend/Socket/Socket.js
+++ backend/Socket/Socket.js
@@ -1,60 +1,44 @@
+// backend/Socket/Socket.js
 import express from "express";
 import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
-let server = null;
-let io = null;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 const userSocketMap = {};
 
-if (process.env.VERCEL !== "1") {
-  server = http.createServer(app);
-  
-  const initializeSocketIO = async () => {
-    try {
-      const { Server } = await import("socket.io");
-      
-      io = new Server(server, {
-        cors: {
-          origin: process.env.CLIENT_URL || "http://localhost:5173",
-          methods: ["GET", "POST"],
-          credentials: true,
-        },
-      });
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
 
-      io.on("connection", (socket) => {
-        const userId = socket.handshake.query.userId;
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
 
-        if (userId) {
-          userSocketMap[userId] = socket.id;
-        }
+  io.emit("getonlineUsers", Object.keys(userSocketMap));
 
-        io.emit("getonlineUsers", Object.keys(userSocketMap));
-
-        socket.on("sendMessage", (newMessage) => {
-          const receiverSocketId = userSocketMap[newMessage.receiverId];
-          if (receiverSocketId) {
-            io.to(receiverSocketId).emit("newMessage", newMessage);
-          }
-        });
-
-        socket.on("disconnect", () => {
-          if (userId) {
-            delete userSocketMap[userId];
-            io.emit("getonlineUsers", Object.keys(userSocketMap));
-          }
-        });
-      });
-    } catch (error) {
-      // Socket.IO initialization failed
+  socket.on("sendMessage", (newMessage) => {
+    const receiverSocketId = userSocketMap[newMessage.receiverId];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
     }
-  };
+  });
 
-  initializeSocketIO().catch(() => {});
-} else {
-  server = { listen: () => {} };
-}
+  socket.on("disconnect", () => {
+    if (userId) {
+      delete userSocketMap[userId];
+      io.emit("getonlineUsers", Object.keys(userSocketMap));
+    }
+  });
+});
 
 export const getReciverSocketId = (receiverId) => userSocketMap[receiverId] || null;
-
 export { io, server, app };
