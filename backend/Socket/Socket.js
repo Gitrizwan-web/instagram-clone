4364cd857f2ace50af1669fa9ecc82e4fac9fcb6
Rizwan Fahim <rizwanfahim011@outlook.com>
2026-01-28 19:23:29 +0500

    Update Socket.js

--- backend/Socket/Socket.js
+++ backend/Socket/Socket.js
@@ -1,4 +1,3 @@
-// backend/Socket/Socket.js
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
@@ -8,7 +7,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin:"https://instagram-clone-bnpm-git-main-gitrizwan-webs-projects.vercel.app/" || "http://localhost:5173",
+    origin: [
+      "https://instagram-clone-bnpm-git-main-gitrizwan-webs-projects.vercel.app",
+      "http://localhost:5173"
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -18,22 +20,22 @@ const userSocketMap = {};
 
 io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
+
   if (userId) userSocketMap[userId] = socket.id;
 
   io.emit("getonlineUsers", Object.keys(userSocketMap));
 
   socket.on("sendMessage", (newMessage) => {
     const receiverSocketId = userSocketMap[newMessage.receiverId];
-    if (receiverSocketId) io.to(receiverSocketId).emit("newMessage", newMessage);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
   });
 
   socket.on("disconnect", () => {
-    if (userId) {
-      delete userSocketMap[userId];
-      io.emit("getonlineUsers", Object.keys(userSocketMap));
-    }
+    if (userId) delete userSocketMap[userId];
+    io.emit("getonlineUsers", Object.keys(userSocketMap));
   });
 });
 
-export const getReciverSocketId = (receiverId) => userSocketMap[receiverId] || null;
-export { io, server, app };
+export { io, server };
